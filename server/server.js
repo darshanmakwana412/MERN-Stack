1ea6f28c50eb7d593125a7c088d3bfa4831b0103
Darshan Makwana <darshanmakwana412@gmail.com>
2022-08-14 14:53:13 +0530

    Made User Schema

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 const app = express();
 
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use((req, res, next)=> {
 })
 
 app.use('/api/posts',postsRoutes);
+app.use('/api/user',userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=> {
